2cf4af29bb8e7a05a73aca4269712287697c18de
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2015-07-05 17:46:20 +0200

    Improved bootloader I2C driver size.

--- Software/Sources/Bootloader/Bootloader.asm
+++ Software/Sources/Bootloader/Bootloader.asm
@@ -2,25 +2,32 @@
 ; @author : Adrien RICCIARDI
 ; @version 1.0 : 19/02/2013
 ; @version 1.1 : 27/02/2013, added I2C EEPROM programming section.
-; @version 1.2 : 19/06/2015, added a waiting loop at the end of the firmware update, merged the release and the development bootloaders into a single one.
+; @version 1.2 : 19/06/2015, added a waiting loop at the end of the firmware update, merged the release and the development bootloaders into a single file.
+; @version 1.3 : 24/06/2015, improved I2C drivers to fasten download time.
 #include <p16f876.inc>
 
+;--------------------------------------------------------------------------------------------------
+; Constants
+;--------------------------------------------------------------------------------------------------
 CODE_PROGRAMMING_START EQU h'FE'
 CODE_ACKNOWLEDGE EQU h'EF'
 CODE_STARTING_PROGRAM EQU 'r' ; Stands for "run"
-ADDRESS_STARTUP EQU d'8025' ; Computed manually...
-EEPROM_BUS_ADDRESS EQU h'A0' ; Address for write commands
+BOOTLOADER_START_ADDRESS EQU d'8045' ; Computed manually...
+EEPROM_WRITE_ADDRESS EQU h'A0' ; I2C EEPROM address with write request
 
+;--------------------------------------------------------------------------------------------------
+; Entry point
+;--------------------------------------------------------------------------------------------------
 	org 0
 	bsf PCLATH, 4 ; Go to program page 3
 	bsf PCLATH, 3
 	goto Bootloader_Start
 
-	org ADDRESS_STARTUP
+	org BOOTLOADER_START_ADDRESS
 Bootloader_Start:
-	;================================================================
+	;==================================================================================================
 	; Initialize UART
-	;================================================================
+	;==================================================================================================
 	bsf STATUS, RP0 ; Go to bank 1
 
 	movlw h'0C' ; Set 19200 bit/s rate
@@ -35,9 +42,9 @@ Bootloader_Start:
 	movlw h'90' ; Serial port enabled, 8-bit, continuous reception enabled
 	movwf RCSTA
 	
-	;================================================================
+	;==================================================================================================
 	; Initialize I2C module
-	;================================================================
+	;==================================================================================================
 	bsf STATUS, RP0 ; Bank 1
 	clrf SSPSTAT ; 400KHz speed mode, I2C input levels
 	clrf SSPCON2 ; Reset communication flags
@@ -47,14 +54,15 @@ Bootloader_Start:
 	bcf STATUS, RP0 ; Bank 0
 	movlw h'28'
 	movwf SSPCON ; Enable I2C module in Master mode
+	bcf PIR1, SSPIF ; Clear the interrupt flag
 	
 	; Reset registers which hold the address
 	clrf h'21'
 	clrf h'22'
 
-	;================================================================
+	;==================================================================================================
 	; Is the start programming code received ?
-	;================================================================
+	;==================================================================================================
 	; Loose approximately 250 * 3 µs to let PC send its code
 	movlw d'251'
 	movwf h'20' ; First available register in bank 0
@@ -79,9 +87,9 @@ Check_Received_Byte:
 	; Light led
 	bsf PORTB, 7
 
-	;================================================================
+	;==================================================================================================
 	; Start programming flash
-	;================================================================
+	;==================================================================================================
 	; Send acknowledge
 	movlw CODE_ACKNOWLEDGE
 	call UARTWriteByte
@@ -93,9 +101,9 @@ Check_Received_Byte:
 	btfsc STATUS, Z
 	goto @Loop_Ignore_Code_Programming_Start
 	
-	;================================================================
+	;==================================================================================================
 	; Receive program instructions
-	;================================================================
+	;==================================================================================================
 Loop_Flash_Program_Instructions:
 	; Address high byte
 	call UARTReadByte 
@@ -111,7 +119,7 @@ Loop_Flash_Program_Instructions:
 	call UARTReadByte
 	bsf STATUS, RP1 ; Bank 2
 	movwf EEADR
-		
+	
 	; Instruction high byte
 	call UARTReadByte
 	bsf STATUS, RP1 ; Bank 2
@@ -122,9 +130,9 @@ Loop_Flash_Program_Instructions:
 	bsf STATUS, RP1 ; Bank 2
 	movwf EEDATA
 	
-	;================================================================
+	;==================================================================================================
 	; Flash received instruction
-	;================================================================
+	;==================================================================================================
 	; Reset end of write flag
 	bcf STATUS, RP1 ; Bank 0
 	bcf PIR2, EEIF
@@ -145,18 +153,18 @@ Loop_Flash_Program_Instructions:
 	nop
 	bcf EECON1, WR ; Write is finished
 
-	;================================================================
+	;==================================================================================================
 	; Send acknowledge to PC
-	;================================================================
+	;==================================================================================================
 	movlw CODE_ACKNOWLEDGE
 	call UARTWriteByte
 	goto Loop_Flash_Program_Instructions
-
-	;================================================================
+	
+	;==================================================================================================
 	; Programming loop of external EEPROM
-	;================================================================
+	;==================================================================================================
 Loop_Program_EEPROM:
-	; Command byte, used only to tell if the program is fully transfered or not
+	; Command byte, used only to tell if the program is fully transferred or not
 	bcf STATUS, RP0 ; Bank 0
 	call UARTReadByte 
 	xorlw CODE_PROGRAMMING_START
@@ -167,103 +175,53 @@ Loop_Program_EEPROM:
 	call UARTReadByte
 	movwf h'20'
 	
-	;================================================================
+	;==================================================================================================
 	; I2C write driver
-	;================================================================
+	;==================================================================================================
 	; Send a start condition to the bus
 	bsf STATUS, RP0 ; Bank 1
-	movlw 1
-	movwf SSPCON2
-	; Wait for start condition completion
-@Loop_Wait_Start_Condition_End:
-	btfsc SSPCON2, SEN
-	goto @Loop_Wait_Start_Condition_End
-		
-	; Send device address and write request
-	bcf STATUS, RP0 ; Bank 0
-	bcf PIR1, SSPIF
-	movlw EEPROM_BUS_ADDRESS
-	movwf SSPBUF
-	; Wait for transmission end
-@Loop_Wait_Device_Address_Transmission:
-	btfss PIR1, SSPIF
-	goto @Loop_Wait_Device_Address_Transmission
+	bsf SSPCON2, SEN
+	call I2CWaitOperationCompletion
 	
-	; Wait for device acknowledge
-	bsf STATUS, RP0 ; Bank 1
-@Loop_Wait_Device_Address_Acknowledge:
-	btfsc SSPCON2, ACKSTAT
-	goto @Loop_Wait_Device_Address_Acknowledge
+	; Send device address and write request
+	movlw EEPROM_WRITE_ADDRESS
+	movwf SSPBUF
+	call I2CWaitOperationCompletion
 	
 	; Send address high byte
-	bcf STATUS, RP0 ; Bank 0
-	bcf PIR1, SSPIF
 	; Get high address byte from memory
 	movf h'22', W
 	movwf SSPBUF
-	; Wait for end of transmission
-@Loop_Wait_Address_High_Byte_Transmission:
-	btfss PIR1, SSPIF
-	goto @Loop_Wait_Address_High_Byte_Transmission
-	
-	; Wait for device acknowledge
-	bsf STATUS, RP0 ; Bank 1
-@Loop_Wait_Address_High_Byte_Acknowledge:
-	btfsc SSPCON2, ACKSTAT
-	goto @Loop_Wait_Address_High_Byte_Acknowledge
+	call I2CWaitOperationCompletion
 	
 	; Send address low byte
-	bcf STATUS, RP0 ; Bank 0
-	bcf PIR1, SSPIF
 	; Get low address byte from memory
 	movf h'21', W
 	movwf SSPBUF
-@Loop_Wait_Address_Low_Byte_Transmission:
-	btfss PIR1, SSPIF
-	goto @Loop_Wait_Address_Low_Byte_Transmission
-	
-	; Wait for device acknowledge
-	bsf STATUS, RP0 ; Bank 1
-@Loop_Wait_Address_Low_Byte_Acknowledge:
-	btfsc SSPCON2, ACKSTAT
-	goto @Loop_Wait_Address_Low_Byte_Acknowledge
+	call I2CWaitOperationCompletion
 	
 	; Send byte to write
-	bcf STATUS, RP0 ; Bank 0
-	bcf PIR1, SSPIF
 	; Retrieve data byte from memory
 	movf h'20', W
 	movwf SSPBUF
-	; Wait for end of transmission
-@Loop_Wait_Data_Byte_Transmission:
-	btfss PIR1, SSPIF
-	goto @Loop_Wait_Data_Byte_Transmission
-	
-	; Wait for device acknowledge
-	bsf STATUS, RP0 ; Bank 1
-@Loop_Wait_Data_Byte_Acknowledge:
-	btfsc SSPCON2, ACKSTAT
-	goto @Loop_Wait_Data_Byte_Acknowledge
+	call I2CWaitOperationCompletion
 	
 	; Send stop condition to start write operation
+	bsf STATUS, RP0 ; Bank 1
 	bsf SSPCON2, PEN
-	; Wait for stop condition completion
-@Loop_Wait_Stop_Condition_End:
-	btfsc SSPCON2, PEN
-	goto @Loop_Wait_Stop_Condition_End
+	call I2CWaitOperationCompletion
 	
-	;================================================================
+	;==================================================================================================
 	; Increment address
-	;================================================================²
-	bcf STATUS, RP0 ; Bank 0
-	incf h'21', F
+	;==================================================================================================
+	incf h'21', F ; We're in bank 0 yet thanks to I2CWaitOperationCompletion
 	; Propagate carry to high byte only if low byte is equal 0 (so it did a roll over)
 	btfsc STATUS, Z
 	incf h'22', F
 	
-	;================================================================
+	;==================================================================================================
 	; Wait for write operation completion
-	;================================================================
+	;==================================================================================================
 	; Each UART byte transmitted at 19200 bit/s halts the program for 520µs.
 	; With 10 calls we wait almost 5.2 ms, which is more than the write time of the EEPROM.
 	; The PC program will ignore received bytes different from CODE_ACKNOWLEDGE.
@@ -279,17 +237,17 @@ Loop_Program_EEPROM:
 	call UARTWriteByte
 	call UARTWriteByte
 	
-	;================================================================
+	;==================================================================================================
 	; Send acknowledge to PC
-	;================================================================
+	;==================================================================================================
 	bcf STATUS, RP0 ; Bank 0
 	movlw CODE_ACKNOWLEDGE
 	call UARTWriteByte
 	goto Loop_Program_EEPROM
-		
-	;================================================================
+	
+	;==================================================================================================
 	; Programming finished
-	;================================================================
+	;==================================================================================================
 Programming_Finished:
 	; Turn led off
 	bcf STATUS, RP0 ; Bank 0
@@ -306,15 +264,14 @@ Programming_Finished:
 	clrf PCLATH
 	goto 0
 
-;----------------------------------------------------------------------------------
+;--------------------------------------------------------------------------------------------------
 ; Functions
-;----------------------------------------------------------------------------------
-;================================================================
+;--------------------------------------------------------------------------------------------------
+;==================================================================================================
 ; Wait until a byte is received from the UART.
 ; @return The received byte.
-; @warning The function selects bank 0 and does not restore
-;          previous bank when returning.
-;================================================================
+; @warning The function selects bank 0 and does not restore previous bank when returning.
+;==================================================================================================
 UARTReadByte:
 	; Go to bank 0
 	bcf STATUS, RP1
@@ -329,13 +286,12 @@ UARTReadByte:
 	movf RCREG, W
 	return
 
-;================================================================
+;==================================================================================================
 ; Wait until the UART transmit line becomes available and send
 ; a byte.
 ; @param Put the byte to transfer in register W.
-; @warning The function selects bank 0 and does not restore
-;          previous bank when returning.
-;================================================================
+; @warning The function selects bank 0 and does not restore previous bank when returning.
+;==================================================================================================
 UARTWriteByte:
 	; Go to bank 0
 	bcf STATUS, RP1
@@ -350,10 +306,27 @@ UARTWriteByte:
 	movwf TXREG
 	return
 
-;----------------------------------------------------------------------------------
-; Jump to the flashed program startup code (it's located at the end of the memory 
-; to ease access to the remapped entry point.
-;----------------------------------------------------------------------------------
+;==================================================================================================
+; Wait until the current I2C operation finishes.
+; @warning The function selects bank 0 and does not restore previous bank when returning.
+;==================================================================================================
+I2CWaitOperationCompletion:
+	; Go to bank 0
+	bcf STATUS, RP1
+	bcf STATUS, RP0
+	
+	; Wait for the interrupt flag to trigger
+@Loop_I2C_Wait_Operation_Completion:
+	btfss PIR1, SSPIF
+	goto @Loop_I2C_Wait_Operation_Completion
+	bcf PIR1, SSPIF
+	
+	return
+
+;--------------------------------------------------------------------------------------------------
+; Jump to the flashed program startup code.
+; It's located at the end of the memory to ease access to the remapped entry point.
+;--------------------------------------------------------------------------------------------------
 	org h'1FF8'
 Start_Program:
 	bcf STATUS, RP1 ; Go to bank 0
@@ -367,9 +340,9 @@ Start_Program:
 	nop
 	nop
 
-;----------------------------------------------------------------------------------
+;--------------------------------------------------------------------------------------------------
 ; Configuration word
-;----------------------------------------------------------------------------------
+;--------------------------------------------------------------------------------------------------
 	__CONFIG _CP_OFF & _DEBUG_OFF & _WRT_ENABLE_ON & _CPD_OFF & _LVP_OFF & _BODEN_ON & _PWRTE_ON  & _WDT_OFF & _XT_OSC
 
 	END
